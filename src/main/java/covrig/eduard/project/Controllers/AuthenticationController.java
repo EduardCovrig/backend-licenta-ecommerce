f1d357be7c6685e4d5a6683f2c5edf40d884497a
Eduard <eduuardcovrig@gmail.com>
2025-12-01 18:38:20 +0200

    A few minor fixes

--- src/main/java/covrig/eduard/project/Controllers/AuthenticationController.java
+++ src/main/java/covrig/eduard/project/Controllers/AuthenticationController.java
@@ -7,13 +7,11 @@ import covrig.eduard.project.dtos.user.UserRegistrationDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin("*")
 public class AuthenticationController {
     private final AuthenticationService service;
 
--- src/main/java/covrig/eduard/project/Security/JwtAuthenticationFilter.java
+++ src/main/java/covrig/eduard/project/Security/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,13 +18,13 @@ import java.io.IOException;
 
 //aceasta clasa se va apela la fiecare request primit de server
 @Component
-@RequiredArgsConstructor //face constructor cu toate cmapurile FINAL
+@RequiredArgsConstructor //face constructor cu toate campurile FINAL
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         final String authHeader = request.getHeader("Authorization"); //cauta headerul de autorizare
         final String jwt; //final in java se aplica odata ce i s-a atribuit o valoare, deci nu ramane final null
